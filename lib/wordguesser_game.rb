ce720e41144c35eae313298e9c668644fe297835
Kaho Furukawa <kaho.f@berkeley.edu>
2026-09-17 13:06:56 -0700

    Implement WordGuesserGame

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,17 +1,50 @@
 class WordGuesserGame
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/wordguesser_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
-  # You can test it by installing irb via $ gem install irb
-  # and then running $ irb -I. -r app.rb
-  # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
-  #  => "cooking"   <-- some random word
+  def guess(letter)
+    unless letter.is_a?(String) && letter.match?(/\A[a-zA-Z]\z/)
+      raise ArgumentError
+    end
+
+    letter = letter.downcase
+
+    # already guessed
+    if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+      return false
+    end
+
+    if @word.downcase.include?(letter)
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+
+    true
+  end
+
+  def word_with_guesses
+    @word.chars.map do |letter|
+      @guesses.include?(letter.downcase) ? letter : '-'
+    end.join
+  end
+
+  def check_win_or_lose
+    if word_with_guesses.downcase == @word.downcase
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
+  end
+
+  # Get a word from remote "random word" service
   def self.get_random_word
     require 'uri'
     require 'net/http'
